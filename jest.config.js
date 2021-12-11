e3b9894f5c05d187f25cc3cbcf0acaf9d621e465
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2021-12-12 00:52:17 +0100

    refacto: tests

--- jest.config.js
+++ jest.config.js
@@ -6,30 +6,9 @@ const { compilerOptions } = require('./tsconfig.json');
 delete compilerOptions.paths['*'];
 
 module.exports = {
-    collectCoverageFrom: ['src/**/*.{ts,js}'],
-    coverageReporters: ['text', 'text-summary'],
-    coverageThreshold: {
-        global: {
-            branches: 0,
-            functions: 0,
-            lines: 0,
-            statements: 0,
-        },
-    },
-    moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
         prefix: '<rootDir>/',
     }),
-    // setupFiles: ['<rootDir>/src/tests/setup/setup.ts'],
-    testEnvironment: 'node',
-    testPathIgnorePatterns: [
-        '/node_modules/',
-        '/build/',
-        '/coverage/',
-        '/scripts/',
-    ],
-    testRegex: '((\\.|/)(test|spec))\\.(js|ts)x?$',
-    transform: {
-        '\\.ts$': 'ts-jest',
-    },
+
+    preset: 'ts-jest',
 };
