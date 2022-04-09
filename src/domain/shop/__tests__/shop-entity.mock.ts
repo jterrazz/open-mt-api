556f5d31c40446f6988eee76ba7899636c382ba3
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2022-04-10 01:27:26 +0200

    feat: user route bootstrap

--- src/domain/shop/__tests__/shop-entity.mock.ts
+++ src/domain/shop/__tests__/shop-entity.mock.ts
@@ -2,9 +2,9 @@ import { IShopEntity } from '@domain/shop/shop-entity';
 
 export const createMockOfShopEntity = (): IShopEntity => ({
     bannerImageUrl: '',
+    countFollowers: 42,
     creationDate: new Date('2020-01-01'),
     description: '',
     handle: '',
     name: '',
-    numberOfFollowers: 42,
 });
