094156d9b026edbd41bc7c2a37a4415befbc7daf
Jean-Baptiste Terrazzoni <jterrazz@icloud.com>
2022-06-19 03:29:52 +0200

    feat: scripts and config

--- scripts/docker-dev.sh
+++ scripts/docker-dev.sh
@@ -2,4 +2,4 @@
 
 source ./scripts/common/docker-setup.sh
 
-docker compose -f scripts/compose/docker-compose.yml up open-market-database open-market-api--dev
+docker compose -f scripts/compose/docker-compose.yml up open-market-database open-market-api--dev -exit-code-from open-market-api--dev
