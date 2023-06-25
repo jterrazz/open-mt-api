dc86a2cda6a24d6a6c0df3880680354fb82fa47b
Jean-Baptiste Terrazzoni <j2aa@proton.me>
2023-06-25 15:55:21 +0200

    [FEATURE] Adds fly.io deployment configuration

--- src/ports/database.ts
+++ src/ports/database.ts
@@ -7,8 +7,6 @@ export interface Database {
 
 export interface UserRepository {
     findById(id: number): Promise<User | null>;
-
     findByEmail(email: string): Promise<User | null>;
-
     create(email: string): Promise<User | null>;
 }
